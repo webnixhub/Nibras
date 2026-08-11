f22cd1edc2dcd3e87e5600eec4d4f0d212712ba9
Webnix <sanibmalik850@gmail.com>
2026-08-11 12:28:54 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import AppNavigator from './src/navigation/AppNavigator';
+import { useNibrasStore } from './src/store/useNibrasStore';
 
 export default function App() {
+  const hasHydrated = useNibrasStore((s) => s.hasHydrated);
+  const resetDailyIfNeeded = useNibrasStore((s) => s.resetDailyIfNeeded);
+
+  useEffect(() => {
+    if (hasHydrated) {
+      resetDailyIfNeeded();
+    }
+  }, [hasHydrated, resetDailyIfNeeded]);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <NavigationContainer>
