38fa68102295f16c11c357cd71c22712fbdfcc94
Webnix <arsh790054@gmail.com>
2026-08-17 14:58:46 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,10 +2,19 @@ import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import * as Sentry from '@sentry/react-native';
 import AppNavigator from './src/navigation/AppNavigator';
 import { useNibrasStore } from './src/store/useNibrasStore';
 
-export default function App() {
+Sentry.init({
+  dsn: 'https://3dcf51a359b9c19404505e767978ff8d@o4511924981465088.ingest.us.sentry.io/4511924984217600',
+  debug: false,
+  tracesSampleRate: 1.0,
+  enableNativeCrashHandling: true,
+  enableAutoSessionTracking: true,
+});
+
+function App() {
   const hasHydrated = useNibrasStore((s) => s.hasHydrated);
   const resetDailyIfNeeded = useNibrasStore((s) => s.resetDailyIfNeeded);
 
@@ -24,3 +33,5 @@ export default function App() {
     </GestureHandlerRootView>
   );
 }
+
+export default Sentry.wrap(App);
